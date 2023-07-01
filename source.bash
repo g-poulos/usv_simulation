58c97edaae234aeffd3f8c7285c1398afa76686a
g-poulos <cs04480@uoi.gr>
2023-07-01 11:58:19 +0300

    added source file

---
+++ source.bash
@@ -0,0 +1,16 @@
+
+export GZ_VERSION=garden
+export GZ_IP=127.0.0.1
+export GZ_SIM_RESOURCE_PATH=\
+$GZ_SIM_RESOURCE_PATH:\
+$HOME/gz_ws/src/asv_wave_sim/gz-waves-models/models:\
+$HOME/gz_ws/src/asv_wave_sim/gz-waves-models/world_models:\
+$HOME/gz_ws/src/asv_wave_sim/gz-waves-models/worlds
+export GZ_SIM_SYSTEM_PLUGIN_PATH=\
+$GZ_SIM_SYSTEM_PLUGIN_PATH:\
+$HOME/gz_ws/install/lib
+export GZ_GUI_PLUGIN_PATH=\
+$GZ_GUI_PLUGIN_PATH:\
+$HOME/gz_ws/src/asv_wave_sim/gz-waves/src/gui/plugins/waves_control/build
+
+source ../install/setup.bash
