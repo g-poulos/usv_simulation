c134ad9e14a846e2ad8a16c9dc1023862a67ffb4
g-poulos <cs04480@uoi.gr>
2023-08-10 20:40:50 +0300

    created boat - works in empty world but not in waves

--- source.bash
+++ source.bash
@@ -5,11 +5,14 @@ export GZ_SIM_RESOURCE_PATH=\
 $GZ_SIM_RESOURCE_PATH:\
 $HOME/gz_ws/src/asv_wave_sim/gz-waves-models/models:\
 $HOME/gz_ws/src/asv_wave_sim/gz-waves-models/world_models:\
-$HOME/gz_ws/src/asv_wave_sim/gz-waves-models/worlds
+$HOME/gz_ws/src/asv_wave_sim/gz-waves-models/worlds:\
+$HOME/gz_ws/src/usv_simulation/models
 
 export GZ_SIM_SYSTEM_PLUGIN_PATH=\
 $GZ_SIM_SYSTEM_PLUGIN_PATH:\
-$HOME/gz_ws/install/lib
+$HOME/gz_ws/install/lib:\
+$HOME/gz_ws/src/usv_simulation/plugins/wind-reader/build
+
 export GZ_GUI_PLUGIN_PATH=\
 $GZ_GUI_PLUGIN_PATH:\
 $HOME/gz_ws/src/asv_wave_sim/gz-waves/src/gui/plugins/waves_control/build
