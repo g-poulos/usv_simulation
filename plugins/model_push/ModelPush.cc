725a167122bb0d14d4fb513eed5515b48e070f7d
g-poulos <cs04480@uoi.gr>
2023-09-18 16:23:46 +0300

    Pushing boat with ModelPush plugin

--- plugins/model_push/ModelPush.cc
+++ plugins/model_push/ModelPush.cc
@@ -1,44 +1,45 @@
-
 #include <string>
 #include <gz/common/Profiler.hh>
 #include <gz/plugin/Register.hh>
 #include <sdf/sdf.hh>
+#include "gz/sim/Model.hh"
+#include "gz/sim/Link.hh"
+
 #include "ModelPush.hh"
 
 using namespace model_push;
 
-//class model_push::ModelPush::Implementation
-//{
-//    public: std::string msg;
-//};
+std::string linkName = "default";
+gz::sim::Link modelLink;
 
-//ModelPush::ModelPush()
-//    : System(), dataPtr(utils::MakeUniqueImpl<Implementation>())
-//{
-//}
 
-//void ModelPush::Configure(const gz::sim::Entity &_entity,
-//                          const std::shared_ptr<const sdf::Element> &_sdf,
-//                          gz::sim::EntityComponentManager &_ecm,
-//                          gz::sim::EventManager &_eventMgr)
-//{
-//
-//}
+ModelPush::ModelPush() {
+}
+
+void ModelPush::Configure(const gz::sim::Entity &_entity,
+                          const std::shared_ptr<const sdf::Element> &_sdf,
+                          gz::sim::EntityComponentManager &_ecm,
+                          gz::sim::EventManager &_eventMgr)
+{
+    gz::sim::Model model(_entity);
+    linkName = _sdf->Get<std::string>("link_name");
+    modelLink = gz::sim::Link(model.LinkByName(_ecm, linkName));
+}
 
 void ModelPush::PreUpdate(const gz::sim::UpdateInfo &_info,
                           gz::sim::EntityComponentManager &_ecm)
 {
-
+    gzmsg << " edw " << linkName << std::endl;
+    modelLink.AddWorldForce(_ecm,
+                            gz::math::Vector3d(1000, 0, 0));
 }
 
 GZ_ADD_PLUGIN(model_push::ModelPush,
               sim::System,
-//              ModelPush::ISystemConfigure,
+              ModelPush::ISystemConfigure,
               ModelPush::ISystemPreUpdate)
 
-//GZ_ADD_PLUGIN_ALIAS(model_push::ModelPush,
-//                    "model_push::ModelPush")
-//
+
 
 
 
--- plugins/model_push/ModelPush.hh
+++ plugins/model_push/ModelPush.hh
@@ -9,17 +9,17 @@ namespace model_push
 {
 	class ModelPush
         : public gz::sim::System,
-//          public gz::sim::ISystemConfigure,
+          public gz::sim::ISystemConfigure,
           public gz::sim::ISystemPreUpdate
     {
-//        public: ModelPush();
-//        public: ~ModelPush() override = default;
+        public: ModelPush();
+        public: ~ModelPush() override = default;
 
-//         // Documentation inherited.
-//        public: void Configure(const gz::sim::Entity &_entity,
-//                               const std::shared_ptr<const sdf::Element> &_sdf,
-//                               gz::sim::EntityComponentManager &_ecm,
-//                               gz::sim::EventManager &_eventMgr) override;
+         // Documentation inherited.
+        public: void Configure(const gz::sim::Entity &_entity,
+                               const std::shared_ptr<const sdf::Element> &_sdf,
+                               gz::sim::EntityComponentManager &_ecm,
+                               gz::sim::EventManager &_eventMgr) override;
 
          // Documentation inherited.
         public: void PreUpdate(const gz::sim::UpdateInfo &_info,
