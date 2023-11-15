8c936ac7ac339ce0311165ba470c72aa2c201342
g-poulos <cs04480@uoi.gr>
2023-11-15 14:30:57 +0200

    Changed how average distance is calculated in torque_dist.py

--- feature_extracion_scripts/torque_dist.py
+++ feature_extracion_scripts/torque_dist.py
@@ -1,46 +1,35 @@
 import pyvista as pv
-import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
 from project_model import compute_draft
 
 
-def get_torque_dist(stl_model, height, draft, num_of_hulls, plot=False):
-    water_level_slice = stl_model.slice(normal='z',
-                                        origin=(0, 0, -(height / 2) + draft))
+def get_torque_dist(stl_model, height, draft, plot=False):
+    water_level_slice = stl_model.clip('z', value=-(height/2)+draft, invert=False)
     center_of_mass = stl_model.center
-    points_2d = water_level_slice.points[:, :2]
-
-    kmeans = KMeans(n_clusters=num_of_hulls, random_state=42)
-    kmeans.fit(points_2d)
-    centers = kmeans.cluster_centers_
-    predicted_labels = kmeans.labels_
+    points = water_level_slice.points
 
     if plot:
-        plt.scatter(points_2d[:, 0], points_2d[:, 1], c=predicted_labels, s=5, cmap='viridis')
-        plt.scatter(centers[:, 0], centers[:, 1], c='red', s=100, marker='X', label='Centers')
-        plt.scatter(center_of_mass[0], center_of_mass[1], c='blue', s=100, marker='o', label='CoM')
-        plt.title("K-Means Clustering")
-        plt.xlabel("X-axis")
-        plt.ylabel("Y-axis")
-        plt.legend()
+        bounds = water_level_slice.bounds
+
+        water_level_slice.plot(style='wireframe')
+        ax = plt.axes(projection="3d")
+        ax.set_xlim(bounds[0], bounds[1])
+        ax.set_ylim(bounds[2], bounds[3])
+        ax.set_zlim(bounds[4], bounds[5])
+
+        ax.scatter3D(points[:, 0], points[:, 1], points[:, 2])
+        ax.scatter3D(center_of_mass[0], center_of_mass[1], center_of_mass[2])
+        ax.plot3D([center_of_mass[0], points[0][0]],
+                  [center_of_mass[1], points[0][1]],
+                  [center_of_mass[2], points[0][2]], c='r')
         plt.show()
 
-    average_distance = np.array([0, 0])
-    center_of_mass_2d = center_of_mass[:2]
+    for point in points:
+        dist = center_of_mass - point
+    avg_dist = dist/len(points)
 
-    if num_of_hulls == 1:
-        for point in points_2d:
-            average_distance = average_distance + (center_of_mass_2d - point)
-        average_distance = average_distance/len(points_2d)
-    else:
-        for cluster_center in centers:
-            average_distance = average_distance + (center_of_mass_2d - cluster_center)
-        average_distance = average_distance/len(centers)
-
-    average_distance = np.append(average_distance, center_of_mass[2])
-
-    return average_distance
+    return avg_dist
 
 
 if __name__ == '__main__':
@@ -51,7 +40,7 @@ if __name__ == '__main__':
     # model_height = 1.5
     # draft = compute_draft(800, 1025, 4.28, 2)
 
-    print(get_torque_dist(poly, model_height, draft, 1, plot=True))
+    print(get_torque_dist(poly, model_height, draft, plot=True))
 
 
 
