Fixed draft calculation

feature_extracion_scripts/compute_draft.py
@@ -1,14 +1,17 @@
 import pyvista as pv
 import numpy as np
+import pymeshfix
 
 
-def compute_draft(stl_file, mass, water_density, plot=False):
+def compute_draft(stl_file, mass, water_density, step_size=0.01, plot=False):
     poly = pv.read(stl_file)
-    height = abs(poly.bounds[5] - poly.bounds[4])
+    min_z = poly.bounds[4]
+    max_z = poly.bounds[5]
     center = poly.center
 
-    for i in np.arange(0.001, poly.bounds[5], 0.001):
-        submerged_part = poly.clip('z', value=(-height/2)+i, invert=True)
+    for i in np.arange(0.001, abs(min_z)+abs(max_z), step_size):
+        submerged_part = poly.clip_closed_surface('-z', origin=(0, 0, min_z+i))
+        submerged_part = submerged_part.clean()
 
         if submerged_part.volume >= mass/water_density:
 
@@ -18,7 +21,8 @@ def compute_draft(stl_file, mass, water_density, plot=False):
 
             if plot:
                 print(f"Draft: {draft}")
-                print(f"Volume: {volume}/{mass/water_density}")
+                print(f"Calculated Volume: {volume}\n"
+                      f"Correct Volume:    {mass/water_density}")
 
                 ground_plane = pv.Plane(center=[center[0], center[1], poly.bounds[4]], i_size=7, j_size=7)
                 water_surface = pv.Plane(center=[center[0], center[1], submerged_part.bounds[5]], i_size=7, j_size=7)
@@ -33,6 +37,6 @@ def compute_draft(stl_file, mass, water_density, plot=False):
 
 
 if __name__ == '__main__':
-    vereniki = "../models/vereniki/meshes/vereniki_scaled.stl"
+    vereniki = "../models/vereniki/meshes/vereniki_scaled3.stl"
     boat = "../models/boat/meshes/boat3.stl"
-    print(compute_draft(vereniki, 425, 1025, plot=True))
+    print(compute_draft(vereniki, 425, 1025, step_size=0.0001, plot=True))

feature_extracion_scripts/vereniki_info.py
@@ -1,6 +1,8 @@
 import numpy as np
-from project_model import project_mesh_to_plane
+from project_model import get_projection_area
 import pyvista as pv
+from scipy.spatial import Delaunay
+import matplotlib.pyplot as plt
 
 
 def h(h_uc, r_uc, m, r_lc, h_lc):
@@ -24,22 +26,18 @@ if __name__ == '__main__':
     print(f"Draft | {6.5 + 3 - h_original}  | {scaled_draft}")
     print(f"Mass  | {m_original}           | {m_scaled}")
 
-    vereniki = pv.read("../models/vereniki/meshes/vereniki_scaled.stl")
+    vereniki = pv.read("../models/vereniki/meshes/vereniki_scaled3.stl")
     vereniki_upper_part = vereniki.clip('z', value=-((0.65+0.3)/2)+scaled_draft, invert=False)
-    vereniki_lower_part = vereniki.clip('z', value=-((0.65+0.3)/2)+scaled_draft, invert=True)
+    vereniki_lower_part = vereniki.clip('z', value=-((0.65+0.3)/2)+0.3, invert=True)
 
-    A_t_c = project_mesh_to_plane(vereniki_lower_part, normal=(1, 0, 0)).area
-    A_l_c = project_mesh_to_plane(vereniki_lower_part, normal=(0, 1, 0)).area
-    # project_mesh_to_plane(vereniki_lower_part, normal=(1, 0, 0)).plot()
-    # project_mesh_to_plane(vereniki_lower_part, normal=(0, 1, 0)).plot()
+    A_t_c = get_projection_area(vereniki_lower_part, normal=(1, 0, 0), plot=False)
+    A_l_c = get_projection_area(vereniki_lower_part, normal=(0, 1, 0), plot=False)
+    print(f"Current Projection: At = {A_t_c},\n"
+          f"                    Al = {A_l_c}")
 
-    print(f"Current Projection: At = {A_t_c}, Al = {A_l_c}")
-
-    A_t_w = project_mesh_to_plane(vereniki_upper_part, normal=(1, 0, 0)).area
-    A_l_w = project_mesh_to_plane(vereniki_upper_part, normal=(0, 1, 0)).area
-    # project_mesh_to_plane(vereniki_upper_part, normal=(1, 0, 0)).plot()
-    # project_mesh_to_plane(vereniki_upper_part, normal=(0, 1, 0)).plot()
-
-    print(f"Wind Projection:    At = {A_t_w}, Al = {A_l_w}")
+    A_t_w = get_projection_area(vereniki_upper_part, normal=(1, 0, 0), plot=False)
+    A_l_w = get_projection_area(vereniki_upper_part, normal=(0, 1, 0), plot=False)
+    print(f"Wind Projection:    At = {A_t_w},\n"
+          f"                    Al = {A_l_w}")
 
 