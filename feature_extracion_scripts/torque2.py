276f0d3cbf3f95627e66708fa55b1764b8dd99a8
g-poulos <cs04480@uoi.gr>
2023-11-29 18:07:45 +0200

    Added toque point output

--- feature_extracion_scripts/torque2.py
+++ feature_extracion_scripts/torque2.py
@@ -19,9 +19,11 @@ def get_part_from_com(mesh, offset):
         side = side * (-1)
 
     cut = mesh.clip_closed_surface(normal=(side, 0, 0), origin=[com[0], 0, 0])
-    cut = make_manifold(cut)
+    if not cut.is_manifold:
+        cut = make_manifold(cut)
     cut = cut.clip_closed_surface(normal=(-side, 0, 0), origin=[com[0]+offset, 0, 0])
-    cut = make_manifold(cut)
+    if not cut.is_manifold:
+        cut = make_manifold(cut)
 
     if not cut.n_points:
         return None
@@ -31,7 +33,6 @@ def get_part_from_com(mesh, offset):
 def check_values(val1, val2, precision=2):
     val1 = round(val1, precision)
     val2 = round(val2, precision)
-    print(val1, val2)
     return val1 == val2
 
 
@@ -67,7 +68,9 @@ def torque_point(mesh, step, plot=False):
                 if check_values(negative_area_torque_value,
                                 positive_area_torque_value,
                                 precision=4-backtrack_num):
-                    break
+                    remaining_part = mesh.clip_closed_surface(normal=(1, 0, 0),
+                                                              origin=[com[0]+pos_offset, 0, 0])
+                    return remaining_part, pos_offset
                 backtrack_num += 1
 
         elif negative_area_torque_value > positive_area_torque_value:
@@ -76,17 +79,25 @@ def torque_point(mesh, step, plot=False):
                 positive_part, positive_area_torque_value = get_torque_value(mesh, pos_offset)
             else:
                 print("Backtracking...")
-                neg_offset = neg_offset - step/2
+                neg_offset = neg_offset - step/10
                 negative_part, negative_area_torque_value = get_torque_value(mesh, -neg_offset)
                 if check_values(negative_area_torque_value,
                                 positive_area_torque_value,
                                 precision=4-backtrack_num):
-                    break
+                    remaining_part = mesh.clip_closed_surface(normal=(-1, 0, 0),
+                                                              origin=[com[0]-neg_offset, 0, 0])
+                    return remaining_part, neg_offset
                 backtrack_num += 1
         else:
             positive_part, positive_area_torque_value = get_torque_value(mesh, pos_offset+step)
             negative_part, negative_area_torque_value = get_torque_value(mesh, -(neg_offset+step))
 
+        if check_values(negative_area_torque_value, positive_area_torque_value, precision=2):
+            if neg_offset >= abs(x_range_negative) and pos_offset >= x_range_positive:
+                print("Equal")
+
+        print(pos_offset, x_range_positive)
+        print(neg_offset, x_range_negative)
         print(f"Total: {positive_area_torque_value} {negative_area_torque_value}")
         if plot:
             axes = pv.Axes(show_actor=True, actor_scale=2.0, line_width=5)
@@ -106,13 +117,21 @@ if __name__ == '__main__':
     draft = 0.45
     vereniki = pv.read("../../../../gz_ws/src/usv_simulation/models/vereniki/meshes/vereniki_scaled3.stl")
     vereniki = vereniki.clip_closed_surface(normal=(0, 0, 1), origin=(0, 0, vereniki.center_of_mass()[2]))
+    com = vereniki.center_of_mass()
+
+    vereniki.rotate_z(120, inplace=True)
+
+    torque_part, offset = torque_point(vereniki, 0.1, plot=False)
+
+    axes = pv.Axes(show_actor=True, actor_scale=2.0, line_width=5)
+    axes.origin = com
 
     p = pv.Plotter()
+    p.add_actor(axes.actor)
     p.add_mesh(vereniki, style='wireframe')
+    p.add_mesh(torque_part)
     p.add_points(np.array([[-1, 0, 0], [0, 0, 0]], dtype=float))
     p.show()
-
-    torque_point(vereniki, 0.1, plot=True)
 
     # FOR DIFFERENT ANGLES
     # axes = pv.Axes(show_actor=True, actor_scale=2.0, line_width=5)
