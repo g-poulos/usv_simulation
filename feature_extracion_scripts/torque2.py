e5451aa76b3b686910c2042c6a209eb273d37c3f
g-poulos <cs04480@uoi.gr>
2023-11-29 12:17:04 +0200

    Splitting parts successively

--- feature_extracion_scripts/torque2.py
+++ feature_extracion_scripts/torque2.py
@@ -11,12 +11,12 @@ def make_manifold(mesh):
     return pv.PolyData(mesh.mesh)
 
 
-def split_from_com(mesh, offset):
+def split_from_com(mesh, neg_offset, pos_offset):
     com = vereniki.center_of_mass()
 
-    cut = mesh.clip_closed_surface(normal=(1, 0, 0), origin=[com[0]-offset, 0, 0])
+    cut = mesh.clip_closed_surface(normal=(1, 0, 0), origin=[com[0]-neg_offset, 0, 0])
     cut = make_manifold(cut)
-    cut = cut.clip_closed_surface(normal=(-1, 0, 0), origin=[com[0]+offset, 0, 0])
+    cut = cut.clip_closed_surface(normal=(-1, 0, 0), origin=[com[0]+pos_offset, 0, 0])
     cut = make_manifold(cut)
 
     if cut.n_points:
@@ -36,32 +36,38 @@ def torque_point(mesh):
     x_range = x_range_positive - x_range_negative
     print(x_range_negative, x_range_positive, x_range)
 
-    offset = 0
-    area1_total = 0
-    area2_total = 0
-    while offset < abs(x_range_negative) and offset < x_range_positive:
-        offset = offset + 0.1
-        rc1, rc2 = split_from_com(mesh, offset)
+    neg_offset = 0.1
+    pos_offset = 0.1
+    negative_area_total = 0
+    positive_area_total = 0
+    step = 0.1
 
-        area1 = get_projection_area(rc1, normal=(0, 1, 0))
-        area2 = get_projection_area(rc2, normal=(0, 1, 0))
-        area1_total = area1_total + area1*offset
-        area2_total = area2_total + area2*offset
-        print(area1, area2)
-        print(f"Total: {area1_total} {area2_total}")
+    while neg_offset < abs(x_range_negative) or pos_offset < x_range_positive:
+        if negative_area_total < positive_area_total:
+            neg_offset = neg_offset + step
+        elif negative_area_total > positive_area_total:
+            pos_offset = pos_offset + step
+
+        positive_part, negative_part = split_from_com(mesh, neg_offset, pos_offset)
+
+        positive_area = get_projection_area(positive_part, normal=(0, 1, 0))
+        negative_area = get_projection_area(negative_part, normal=(0, 1, 0))
+        positive_area_total = positive_area_total + positive_area*pos_offset
+        negative_area_total = negative_area_total + negative_area*neg_offset
+
+        print(f"Total: {negative_area_total} {positive_area_total}")
 
         axes = pv.Axes(show_actor=True, actor_scale=2.0, line_width=5)
         axes.origin = com
         p = pv.Plotter()
         p.set_background('grey', 'black')
         p.add_mesh(mesh, style='wireframe')
-        p.add_mesh(rc1, color='g')
-        p.add_mesh(rc2, color='r')
-        p.add_text(f"Cut1: {rc1.is_manifold}, Cut2: {rc2.is_manifold}", color='w')
+        p.add_mesh(positive_part, color='g')
+        p.add_mesh(negative_part, color='r')
+        p.add_text(f"Cut1: {positive_part.is_manifold}, Cut2: {negative_part.is_manifold}", color='w')
         p.add_actor(axes.actor)
         p.add_points(np.array(vereniki.center_of_mass()), color='r')
         p.show()
-
 
 
 if __name__ == '__main__':
