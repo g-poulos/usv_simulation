d39727ab8be00ff8fa40a745700800b14121b40a
g-poulos <cs04480@uoi.gr>
2023-11-29 17:09:26 +0200

    Refactored torque_point

--- feature_extracion_scripts/torque2.py
+++ feature_extracion_scripts/torque2.py
@@ -35,17 +35,20 @@ def check_values(val1, val2, precision=2):
     return val1 == val2
 
 
-def torque_point(mesh):
+def get_torque_value(mesh, offset):
+    part = get_part_from_com(mesh, offset)
+    area = get_projection_area(part, normal=(0, 1, 0))
+    return part, area*abs(offset)
+
+
+def torque_point(mesh, step, plot=False):
     bounds = mesh.bounds[:2]
     com = mesh.center_of_mass()
     x_range_negative = bounds[0] + com[0]
     x_range_positive = bounds[1] + com[0]
-    x_range = x_range_positive - x_range_negative
-    print(x_range_negative, x_range_positive, x_range)
 
-    step = 0.1
-    neg_offset = step
-    pos_offset = step
+    neg_offset = 0
+    pos_offset = 0
     negative_area_torque_value = 0
     positive_area_torque_value = 0
     positive_part = None
@@ -56,15 +59,11 @@ def torque_point(mesh):
         if negative_area_torque_value < positive_area_torque_value:
             if neg_offset <= abs(x_range_negative):
                 neg_offset = neg_offset + step
-                negative_part = get_part_from_com(mesh, -neg_offset)
-                negative_area = get_projection_area(negative_part, normal=(0, 1, 0))
-                negative_area_torque_value = negative_area*neg_offset
+                negative_part, negative_area_torque_value = get_torque_value(mesh, -neg_offset)
             else:
                 print("Backtracking...")
                 pos_offset = pos_offset - step/10
-                positive_part = get_part_from_com(mesh, pos_offset)
-                positive_area = get_projection_area(positive_part, normal=(0, 1, 0))
-                positive_area_torque_value = positive_area*pos_offset
+                positive_part, positive_area_torque_value = get_torque_value(mesh, pos_offset)
                 if check_values(negative_area_torque_value,
                                 positive_area_torque_value,
                                 precision=4-backtrack_num):
@@ -74,37 +73,33 @@ def torque_point(mesh):
         elif negative_area_torque_value > positive_area_torque_value:
             if pos_offset <= x_range_positive:
                 pos_offset = pos_offset + step
-                positive_part = get_part_from_com(mesh, pos_offset)
-                positive_area = get_projection_area(positive_part, normal=(0, 1, 0))
-                positive_area_torque_value = positive_area*pos_offset
+                positive_part, positive_area_torque_value = get_torque_value(mesh, pos_offset)
             else:
                 print("Backtracking...")
                 neg_offset = neg_offset - step/2
-                negative_part = get_part_from_com(mesh, -neg_offset)
-                negative_area = get_projection_area(negative_part, normal=(0, 1, 0))
-                negative_area_torque_value = negative_area*neg_offset
-                if check_values(negative_area_torque_value, positive_area_torque_value):
+                negative_part, negative_area_torque_value = get_torque_value(mesh, -neg_offset)
+                if check_values(negative_area_torque_value,
+                                positive_area_torque_value,
+                                precision=4-backtrack_num):
                     break
+                backtrack_num += 1
         else:
-            negative_part = get_part_from_com(mesh, -neg_offset)
-            positive_part = get_part_from_com(mesh, pos_offset)
-            negative_area = get_projection_area(negative_part, normal=(0, 1, 0))
-            negative_area_torque_value += negative_area*neg_offset
-            positive_area = get_projection_area(positive_part, normal=(0, 1, 0))
-            positive_area_torque_value += positive_area*pos_offset
+            positive_part, positive_area_torque_value = get_torque_value(mesh, pos_offset+step)
+            negative_part, negative_area_torque_value = get_torque_value(mesh, -(neg_offset+step))
 
         print(f"Total: {positive_area_torque_value} {negative_area_torque_value}")
-        axes = pv.Axes(show_actor=True, actor_scale=2.0, line_width=5)
-        axes.origin = com
-        p = pv.Plotter()
-        p.set_background('grey', 'black')
-        p.add_mesh(mesh, style='wireframe')
-        p.add_mesh(positive_part, color='g')
-        p.add_mesh(negative_part, color='r')
-        p.add_text(f"Cut1: {positive_part.is_manifold}, Cut2: {negative_part.is_manifold}", color='w')
-        p.add_actor(axes.actor)
-        p.add_points(np.array(vereniki.center_of_mass()), color='r')
-        p.show()
+        if plot:
+            axes = pv.Axes(show_actor=True, actor_scale=2.0, line_width=5)
+            axes.origin = com
+            p = pv.Plotter()
+            p.set_background('grey', 'black')
+            p.add_mesh(mesh, style='wireframe')
+            p.add_mesh(positive_part, color='g')
+            p.add_mesh(negative_part, color='r')
+            p.add_text(f"Cut1: {positive_part.is_manifold}, Cut2: {negative_part.is_manifold}", color='w')
+            p.add_actor(axes.actor)
+            p.add_points(np.array(vereniki.center_of_mass()), color='r')
+            p.show()
 
 
 if __name__ == '__main__':
@@ -117,7 +112,7 @@ if __name__ == '__main__':
     p.add_points(np.array([[-1, 0, 0], [0, 0, 0]], dtype=float))
     p.show()
 
-    torque_point(vereniki)
+    torque_point(vereniki, 0.1, plot=True)
 
     # FOR DIFFERENT ANGLES
     # axes = pv.Axes(show_actor=True, actor_scale=2.0, line_width=5)
